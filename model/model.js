7a74e08c983b1cb817505613863bb759413fc6f3
braynwash <f.tzgeraldz@gmail.com>
2023-04-22 20:41:45 -0400

    Made nav elements change color when new page loads

--- model/model.js
+++ model/model.js
@@ -13,15 +13,25 @@ if (pageID != "") {
         $("#app").html(data);
         $('nav').addClass('nav-white'); 
         $('nav').removeClass('nav-black');
+        $('span').addClass('logo-white'); 
+        $('span').removeClass('logo-black'); 
+        $('#links').removeClass('a-black'); 
     })
     }
     if (pageID == pages[0]) {
     $('nav').addClass('nav-white');  
     $('nav').removeClass('nav-black');
+    $('span').addClass('logo-white'); 
+    $('span').removeClass('logo-black'); 
+    $('#links').addClass('a-white'); 
+    $('#links').removeClass('a-black'); 
     // console.log(pageID);
 } else {
     $('nav').removeClass('nav-white'); 
     $('nav').addClass('nav-black'); 
+    $('span').removeClass('logo-white'); 
+    $('span').addClass('logo-black'); 
+    $('#links').addClass('a-black'); 
     // console.log(pageID);
 }
 }
